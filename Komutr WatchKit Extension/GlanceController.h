76056ae050679e14bcd86fb5b966c4fc80bf3a4f
Ray P <rtepowers@gmail.com>
2014-11-23 13:20:32 -0800

    Fixed huge bug

---
+++ Komutr WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  Komutr WatchKit Extension
+//
+//  Created by Raymond Powers on 11/23/14.
+//  Copyright (c) 2014 RayAndDaniel. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ Komutr WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Komutr WatchKit Extension
+//
+//  Created by Raymond Powers on 11/23/14.
+//  Copyright (c) 2014 RayAndDaniel. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
