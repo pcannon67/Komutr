bd74ed3ae94f3a1061488f12b9abd0383bcc8f7d
Ray P <rtepowers@gmail.com>
2014-11-23 14:23:34 -0800

    more art and style

--- Komutr Watch App/KMTRNorthboundInterfaceController.m
+++ Komutr Watch App/KMTRNorthboundInterfaceController.m
@@ -28,7 +28,7 @@
 
 - (void) willActivate
 {
-	mountainViewCaltrainTimesNorthboundWeekend = [NSArray arrayWithObjects:@"8:19am",@"9:19am", @"10:19am", @"11:19am", @"12:19pm", @"1:19pm", @"2:19pm", @"3:19pm", @"4:19pm", @"5:19pm", @"6:19pm", @"7:19pm", @"8:19pm", @"9:19pm", nil]
+	mountainViewCaltrainTimesNorthboundWeekend = [NSArray arrayWithObjects:@"8:19am",@"9:19am", @"10:19am", @"11:19am", @"12:19pm", @"1:19pm", @"2:19pm", @"3:19pm", @"4:19pm", @"5:19pm", @"6:19pm", @"7:19pm", @"8:19pm", @"9:19pm", nil];
 	
 }
 
